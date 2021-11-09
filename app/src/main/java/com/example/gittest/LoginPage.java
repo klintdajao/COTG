d6be98f04b054f08c94b4c23038dc4ff6edf06bd
Reynald Sampelo <reynaldsampelo6@gmail.com>
2021-11-09 22:53:13 +0800

    login page new and about us

--- app/src/main/java/com/example/gittest/LoginPage.java
+++ app/src/main/java/com/example/gittest/LoginPage.java
@@ -4,11 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class AboutUs extends AppCompatActivity {
+public class LoginPage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_about_us);
+        setContentView(R.layout.activity_login_page);
+        getSupportActionBar().hide();
     }
 }
