0a679d07b65e3109f2bebc6b73bf15a76c3fd545
Totsukki <93961776+Totsukki@users.noreply.github.com>
2021-11-10 21:39:01 +0800

    initial commit

--- app/src/main/java/com/example/gittest/Register.java
+++ app/src/main/java/com/example/gittest/Register.java
@@ -3,13 +3,19 @@ package com.example.gittest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class Register extends AppCompatActivity {
-
+    Button btnRegister;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
         getSupportActionBar().hide();
+        btnRegister = findViewById(R.id.btnRegister);
+
+        btnRegister.setAlpha(.3f);
+        btnRegister.setEnabled(false);
+
     }
 }
