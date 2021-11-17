d302eddd01a583c66d5731e51c853edec75f2a09
Totsukki <93961776+Totsukki@users.noreply.github.com>
2021-11-17 20:03:14 +0800

    login.java button fixes

--- app/src/main/java/com/example/gittest/Login.java
+++ app/src/main/java/com/example/gittest/Login.java
@@ -11,7 +11,7 @@ import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
-public class Login extends AppCompatActivity implements View.OnClickListener{
+public class Login extends AppCompatActivity{
     EditText id, pass;
     TextView forgot;
     Button btnActivity1;
@@ -23,31 +23,30 @@ public class Login extends AppCompatActivity implements View.OnClickListener{
         setContentView(R.layout.activity_login);
         getSupportActionBar().hide();
 
+        id = findViewById(R.id.txtlogin);
+        pass = findViewById(R.id.txtpassword);
+
         btnActivity1 = (Button)findViewById(R.id.btnlogin);
         forgot= findViewById(R.id.forgot);
 
-        btnActivity1.setOnClickListener(this);
-
-    }
-
-    @Override
-    public void onClick(View v) {
-
-        switch(v.getId()){
-            case R.id.btnlogin:
-                mydb= new DatabaseHelper(this);
-                String idnum= id.getText().toString();
-                String password= pass.getText().toString();
-                if(idnum.equals("")|| password.equals("")){
-                    Toast.makeText(Login.this, "Enter Username or Password", Toast.LENGTH_SHORT).show();
+        btnActivity1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switch (v.getId()) {
+                    case R.id.btnlogin:
+                        String idnum = id.getText().toString();
+                        String password = pass.getText().toString();
+                        if (idnum.equals("") || password.equals("")) {
+                            Toast.makeText(Login.this, "Enter Username or Password", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(Login.this, "Welcome!", Toast.LENGTH_SHORT).show();
+                        }
+                        break;
+                    default:
+                        throw new IllegalStateException("Unexpected value: " + v.getId());
                 }
-                else{
-                        Toast.makeText(Login.this, "Welcome!", Toast.LENGTH_SHORT).show();
-                }
-                break;
-            default:
-                throw new IllegalStateException("Unexpected value: " + v.getId());
+            }
+        });
+
         }
-
-    }
 }
