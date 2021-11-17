bfd9827656d5223a9e8514d4a40f6c8d0198aa23
Reynald Sampelo <reynaldsampelo6@gmail.com>
2021-11-17 18:07:43 +0800

    register layout and shit by gemarlo

--- app/src/main/java/com/example/gittest/Register.java
+++ app/src/main/java/com/example/gittest/Register.java
@@ -2,7 +2,12 @@ package com.example.gittest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
+import android.graphics.Rect;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -10,8 +15,8 @@ import android.widget.Toast;
 
 public class Register extends AppCompatActivity {
     DatabaseHelper db;
-    EditText editId, editEmail, editFN, editMN, editLN,editPassword1, editPassword2;
-    Button register;
+    EditText idnum, email, fname, mname, lname, password, confPass;
+    Button btnRegister;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,33 +24,190 @@ public class Register extends AppCompatActivity {
         getSupportActionBar().hide();
         db = new DatabaseHelper(this);
 
-        editId = (EditText) findViewById(R.id.editTextID);
-        editEmail = findViewById(R.id.editTextEmail);
-        editFN = findViewById(R.id.editTextFirstName);
-        editMN = findViewById(R.id.editTextMiddleName);
-        editLN = findViewById(R.id.editTextLastName);
-        editPassword1 = findViewById(R.id.editTextPassword1);
-        editPassword2 = findViewById(R.id.editTextPassword2);
-        register = findViewById(R.id.btnRegister);
+        btnRegister = findViewById(R.id.btnRegister);
+        idnum = findViewById(R.id.txtIdNum);
+        email = findViewById(R.id.txtEmail);
+        fname = findViewById(R.id.txtFName);
+        mname = findViewById(R.id.txtMName);
+        lname = findViewById(R.id.txtLName);
+        password = findViewById(R.id.txtPassword);
+        confPass = findViewById(R.id.txtConfPass);
 
-        addUser();
+        btnRegister.setBackgroundColor(Color.LTGRAY);
+        btnRegister.setEnabled(false);
+
+        Drawable checkIcon;
+        checkIcon = getResources().getDrawable(R.drawable.ic_baseline_check_circle_24);
+        checkIcon.setBounds(new Rect(0, 0, checkIcon.getIntrinsicWidth(), checkIcon.getIntrinsicHeight()));
+
+        idnum.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if(idnum.length() <= 0){
+                    idnum.setError("This field cannot be blank");
+                }
+                else{
+                    idnum.setError(null);
+                }
+            }
+        });
+
+        email.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if(email.length() <= 0){
+                    email.setError("This field cannot be blank");
+                }
+                else{
+                    email.setError(null);
+                }
+            }
+        });
+
+        fname.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if(fname.length() <= 0){
+                    fname.setError("This field cannot be blank");
+                }
+                else{
+                    fname.setError(null);
+                }
+            }
+        });
+
+        mname.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if(mname.length() <= 0){
+                    mname.setError("This field cannot be blank");
+                }
+                else{
+                    mname.setError(null);
+                }
+            }
+        });
+
+        lname.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if(lname.length() <= 0){
+                    lname.setError("This field cannot be blank");
+                }
+                else{
+                    lname.setError(null);
+                }
+            }
+        });
+
+        password.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if(password.length() <= 0){
+                    password.setError("This field cannot be blank");
+                }
+                else{
+                    password.setError(null);
+                }
+            }
+        });
+
+        confPass.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if(confPass.length() <= 0){
+                    confPass.setError("This field cannot be blank");
+                    btnRegister.setBackgroundColor(Color.LTGRAY);
+                    btnRegister.setEnabled(false);
+                }
+                else{
+                    if (password.getText().toString().equals(confPass.getText().toString())){
+                        btnRegister.setBackgroundColor(Color.parseColor("#FFCC33"));
+                        btnRegister.setEnabled(true);
+                        confPass.setError("Password match",checkIcon);
+                        password.setError("Password match", checkIcon);
+                    }else {
+                        confPass.setError("Password does not match");
+                        btnRegister.setBackgroundColor(Color.LTGRAY);
+                        btnRegister.setEnabled(false);
+
+                    }
+                }
+            }
+        });
     }
 
     public void addUser(){
-        register.setOnClickListener(
+        btnRegister.setOnClickListener(
                 new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
                         boolean insert=false;
-                        String id = editId.getText().toString();
-                        String email = editEmail.getText().toString();
-                        String fn = editFN.getText().toString();
-                        String mn = editMN.getText().toString();
-                        String ln = editLN.getText().toString();
-                        String p1 = editPassword1.getText().toString();
-                        String p2 = editPassword2.getText().toString();
+                        String id = idnum.getText().toString();
+                        String e = email.getText().toString();
+                        String fn = fname.getText().toString();
+                        String mn = mname.getText().toString();
+                        String ln = lname.getText().toString();
+                        String p1 = password.getText().toString();
+                        String p2 = confPass.getText().toString();
                         if(p1.equals(p2)) {
-                            insert = db.addUser(id, email, fn, mn, ln, p2);
+                            insert = db.addUser(id, e, fn, mn, ln, p2);
 
                             if (insert == true)
                                 Toast.makeText(Register.this, "Data Inserted", Toast.LENGTH_SHORT).show();
@@ -54,17 +216,17 @@ public class Register extends AppCompatActivity {
                         }
                         else{
                             Toast.makeText(Register.this, "Passwords do not match", Toast.LENGTH_SHORT).show();
-                            editPassword1.setText("");
-                            editPassword2.setText("");
+                            password.setText("");
+                            confPass.setText("");
                         }
 
-                        editId.setText("");
-                        editEmail.setText("");
-                        editFN.setText("");
-                        editMN.setText("");
-                        editLN.setText("");
-                        editPassword1.setText("");
-                        editPassword2.setText("");
+                        idnum.setText("");
+                        email.setText("");
+                        fname.setText("");
+                        mname.setText("");
+                        lname.setText("");
+                        password.setText("");
+                        confPass.setText("");
                     }
                 }
         );
