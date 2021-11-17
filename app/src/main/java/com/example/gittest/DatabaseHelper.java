04be311d6f078fc5b3a333d82ed1eabc62902edb
Reynald Sampelo <reynaldsampelo6@gmail.com>
2021-11-17 16:30:12 +0800

    change variable names

--- app/src/main/java/com/example/gittest/DatabaseHelper.java
+++ app/src/main/java/com/example/gittest/DatabaseHelper.java
@@ -7,14 +7,14 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 
 public class DatabaseHelper extends SQLiteOpenHelper{
-    public static final String DATABASE_NAME = "account.db";
-    public static final String TABLE_NAME = "account_table";
-    public static final String COL_1 = "ID";
-    public static final String COL_2 = "EMAIL";
-    public static final String COL_3 = "FIRSTNAME";
-    public static final String COL_4 = "MIDDLENAME";
-    public static final String COL_5 = "SURNAME";
-    public static final String COL_6 = "PASSWORD";
+    public static final String DATABASE_NAME = "cotg.db";
+    public static final String ACCOUNT_TABLE_NAME = "account_table";
+    public static final String ACCOUNT_COL_1 = "ID";
+    public static final String ACCOUNT_COL_2 = "EMAIL";
+    public static final String ACCOUNT_COL_3 = "FIRSTNAME";
+    public static final String ACCOUNT_COL_4 = "MIDDLENAME";
+    public static final String ACCOUNT_COL_5 = "SURNAME";
+    public static final String ACCOUNT_COL_6 = "PASSWORD";
 
     public DatabaseHelper(Context context) {
         super(context, DATABASE_NAME , null, 1);
@@ -23,25 +23,25 @@ public class DatabaseHelper extends SQLiteOpenHelper{
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL("create table " + TABLE_NAME + "(ID TEXT PRIMARY KEY, EMAIL TEXT, FIRSTNAME TEXT, MIDDLENAME TEXT, SURNAME TEXT, PASSWORD TEXT)");
+        db.execSQL("create table " + ACCOUNT_TABLE_NAME + "(ID TEXT PRIMARY KEY, EMAIL TEXT, FIRSTNAME TEXT, MIDDLENAME TEXT, SURNAME TEXT, PASSWORD TEXT)");
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int i, int i1) {
-        db.execSQL(" DROP TABLE IF EXISTS "+TABLE_NAME);
+        db.execSQL(" DROP TABLE IF EXISTS "+ACCOUNT_TABLE_NAME);
         onCreate(db);
     }
 
     public boolean addUser(String id, String email, String fn, String mn, String ln, String p){
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues cv = new ContentValues();
-        cv.put(COL_1,id);
-        cv.put(COL_2,email);
-        cv.put(COL_3,fn);
-        cv.put(COL_4,mn);
-        cv.put(COL_5,ln);
-        cv.put(COL_6,p);
-        long result = db.insert(TABLE_NAME,null,cv);
+        cv.put(ACCOUNT_COL_1,id);
+        cv.put(ACCOUNT_COL_2,email);
+        cv.put(ACCOUNT_COL_3,fn);
+        cv.put(ACCOUNT_COL_4,mn);
+        cv.put(ACCOUNT_COL_5,ln);
+        cv.put(ACCOUNT_COL_6,p);
+        long result = db.insert(ACCOUNT_TABLE_NAME,null,cv);
         if(result == -1)
             return false;
         else
