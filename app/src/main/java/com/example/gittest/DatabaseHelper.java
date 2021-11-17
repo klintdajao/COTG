6809283d4f25e703c6968ce98faf7f1f9e844ac2
Reynald Sampelo <reynaldsampelo6@gmail.com>
2021-11-17 16:26:32 +0800

    Create feature

--- app/src/main/java/com/example/gittest/DatabaseHelper.java
+++ app/src/main/java/com/example/gittest/DatabaseHelper.java
@@ -1,5 +1,6 @@
 package com.example.gittest;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
@@ -22,12 +23,28 @@ public class DatabaseHelper extends SQLiteOpenHelper{
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL("create table " + TABLE_NAME + "(ID INTEGER PRIMARY KEY, EMAIL TEXT, FIRSTNAME TEXT, MIDDLENAME TEXT, SURNAME TEXT, PASSWORD TEXT)");
+        db.execSQL("create table " + TABLE_NAME + "(ID TEXT PRIMARY KEY, EMAIL TEXT, FIRSTNAME TEXT, MIDDLENAME TEXT, SURNAME TEXT, PASSWORD TEXT)");
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int i, int i1) {
         db.execSQL(" DROP TABLE IF EXISTS "+TABLE_NAME);
         onCreate(db);
+    }
+
+    public boolean addUser(String id, String email, String fn, String mn, String ln, String p){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues cv = new ContentValues();
+        cv.put(COL_1,id);
+        cv.put(COL_2,email);
+        cv.put(COL_3,fn);
+        cv.put(COL_4,mn);
+        cv.put(COL_5,ln);
+        cv.put(COL_6,p);
+        long result = db.insert(TABLE_NAME,null,cv);
+        if(result == -1)
+            return false;
+        else
+            return true;
     }
 }
--- app/src/main/java/com/example/gittest/Register.java
+++ app/src/main/java/com/example/gittest/Register.java
@@ -3,14 +3,70 @@ package com.example.gittest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class Register extends AppCompatActivity {
     DatabaseHelper db;
+    EditText editId, editEmail, editFN, editMN, editLN,editPassword1, editPassword2;
+    Button register;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
         getSupportActionBar().hide();
         db = new DatabaseHelper(this);
+
+        editId = (EditText) findViewById(R.id.editTextID);
+        editEmail = findViewById(R.id.editTextEmail);
+        editFN = findViewById(R.id.editTextFirstName);
+        editMN = findViewById(R.id.editTextMiddleName);
+        editLN = findViewById(R.id.editTextLastName);
+        editPassword1 = findViewById(R.id.editTextPassword1);
+        editPassword2 = findViewById(R.id.editTextPassword2);
+        register = findViewById(R.id.btnRegister);
+
+        addUser();
+    }
+
+    public void addUser(){
+        register.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        boolean insert=false;
+                        String id = editId.getText().toString();
+                        String email = editEmail.getText().toString();
+                        String fn = editFN.getText().toString();
+                        String mn = editMN.getText().toString();
+                        String ln = editLN.getText().toString();
+                        String p1 = editPassword1.getText().toString();
+                        String p2 = editPassword2.getText().toString();
+                        if(p1.equals(p2)) {
+                            insert = db.addUser(id, email, fn, mn, ln, p2);
+
+                            if (insert == true)
+                                Toast.makeText(Register.this, "Data Inserted", Toast.LENGTH_SHORT).show();
+                            else
+                                Toast.makeText(Register.this, "Data not inserted", Toast.LENGTH_SHORT).show();
+                        }
+                        else{
+                            Toast.makeText(Register.this, "Passwords do not match", Toast.LENGTH_SHORT).show();
+                            editPassword1.setText("");
+                            editPassword2.setText("");
+                        }
+
+                        editId.setText("");
+                        editEmail.setText("");
+                        editFN.setText("");
+                        editMN.setText("");
+                        editLN.setText("");
+                        editPassword1.setText("");
+                        editPassword2.setText("");
+                    }
+                }
+        );
     }
 }
