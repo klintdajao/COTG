b6b1dd0613efda1bbcbece58a6526b670f7bfb3b
Totsukki <93961776+Totsukki@users.noreply.github.com>
2021-11-17 18:44:29 +0800

    commit and shites

--- app/src/main/java/com/example/gittest/Register.java
+++ app/src/main/java/com/example/gittest/Register.java
@@ -2,9 +2,12 @@ package com.example.gittest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.graphics.Rect;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -13,7 +16,7 @@ import android.widget.Toast;
 public class Register extends AppCompatActivity {
     DatabaseHelper db;
     EditText editId, editEmail, editFN, editMN, editLN,editPassword1, editPassword2;
-    Button register;
+    Button btnRegister;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,7 +31,7 @@ public class Register extends AppCompatActivity {
         editLN = findViewById(R.id.editTextLastName);
         editPassword1 = findViewById(R.id.editTextPassword1);
         editPassword2 = findViewById(R.id.editTextPassword2);
-        register = findViewById(R.id.btnRegister);
+        btnRegister = findViewById(R.id.btnRegister);
 
         addUser();
 
@@ -36,11 +39,164 @@ public class Register extends AppCompatActivity {
         checkIcon = getResources().getDrawable(R.drawable.ic_baseline_check_circle_24);
         checkIcon.setBounds(new Rect(0, 0, checkIcon.getIntrinsicWidth(), checkIcon.getIntrinsicHeight()));
 
+        btnRegister.setBackgroundColor(Color.LTGRAY);
+
+        editId.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if(editId.length() <= 0){
+                    editId.setError("This field cannot be blank");
+                }
+                else{
+                    editId.setError(null);
+                }
+            }
+        });
+        editEmail.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if(editEmail.length() <= 0){
+                    editEmail.setError("This field cannot be blank");
+                }
+                else{
+                    editEmail.setError(null);
+                }
+            }
+        });
+
+        editFN.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if(editFN.length() <= 0){
+                    editFN.setError("This field cannot be blank");
+                }
+                else{
+                    editFN.setError(null);
+                }
+            }
+        });
+
+        editMN.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if(editMN.length() <= 0){
+                    editMN.setError("This field cannot be blank");
+                }
+                else{
+                    editMN.setError(null);
+                }
+            }
+        });
+
+        editLN.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if(editLN.length() <= 0){
+                    editLN.setError("This field cannot be blank");
+                }
+                else{
+                    editLN.setError(null);
+                }
+            }
+        });
+
+        editPassword1.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if(editPassword1.length() <= 0){
+                    editPassword1.setError("This field cannot be blank");
+                }
+                else{
+                    editPassword1.setError(null);
+                }
+            }
+        });
+
+        editPassword1.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if(editPassword2.length() <= 0){
+                    editPassword2.setError("This field cannot be blank");
+                    btnRegister.setBackgroundColor(Color.LTGRAY);
+                    btnRegister.setEnabled(false);
+                }
+                else{
+                    if (editPassword1.getText().toString().equals(editPassword2.getText().toString())){
+                        btnRegister.setBackgroundColor(Color.parseColor("#ffcc33"));
+                        btnRegister.setEnabled(true);
+                        editPassword1.setError("Password match",checkIcon);
+                        editPassword2.setError("Password match", checkIcon);
+                    }else {
+                        editPassword1.setError("Password does not match");
+                        editPassword2.setError("Password does not match");
+                        btnRegister.setBackgroundColor(Color.LTGRAY);
+                        btnRegister.setEnabled(false);
+
+                    }
+                }
+            }
+        });
 
     }
 
     public void addUser(){
-        register.setOnClickListener(
+        btnRegister.setOnClickListener(
                 new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
