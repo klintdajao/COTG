ae2435be08d5ff37e5bf9816c7dee6ae54bc68d7
Totsukki <93961776+Totsukki@users.noreply.github.com>
2021-11-18 21:21:57 +0800

    fragment_cart updates

--- app/src/main/java/com/example/gittest/DatabaseHelper.java
+++ app/src/main/java/com/example/gittest/DatabaseHelper.java
@@ -128,7 +128,7 @@ public class DatabaseHelper extends SQLiteOpenHelper{
         ContentValues contentValues = new ContentValues();
         contentValues.put(CART_COL_3, quantity);
 
-        return db.update(CART_TABLE_NAME, contentValues, ACCOUNT_COL_1 + "=" + "= '"+userid+"'" + " AND " + CART_COL_2 + "= '"+ordername+"'", null)>0;
+        return db.update(CART_TABLE_NAME, contentValues, ACCOUNT_COL_1 + "= '"+userid+"'" + " AND " + CART_COL_2 + "= '"+ordername+"'", null)>0;
     }
 
     public ArrayList<String> checkCartList(String userid){
@@ -173,11 +173,11 @@ public class DatabaseHelper extends SQLiteOpenHelper{
     public boolean deleteCartEntry(String userid, String ordername) {
         SQLiteDatabase db = this.getWritableDatabase();
         boolean result = false;
-        String query = "Select * from cart_table where ID = " + userid;
+        String query = "Select * from cart_table where ID " + "'" + userid + "'";
         Cursor cursor = db.rawQuery(query, null);
         if (cursor.moveToFirst()) {
             cursor.getString(0);
-            db.delete("tblCart", "ordername = ?", new String[]{ordername});
+            db.delete("cart_table", "ordername = ?", new String[]{ordername});
             cursor.close();
             result = true;
         }
@@ -187,10 +187,11 @@ public class DatabaseHelper extends SQLiteOpenHelper{
     public boolean deleteCart(String userid) {
         SQLiteDatabase db = this.getWritableDatabase();
         boolean result = false;
-        String query = "Select * from cart_table where ID= " + userid;
+        String query = "Select * from cart_table where ID =" + "'" + userid + "'";
+
         Cursor cursor = db.rawQuery(query, null);
         if (cursor.moveToFirst()) {
-            db.delete("tblCart", "userid = ?", new String[]{userid});
+            db.delete("cart_table", "ID = ?", new String[]{userid});
             cursor.close();
             result = true;
         }
