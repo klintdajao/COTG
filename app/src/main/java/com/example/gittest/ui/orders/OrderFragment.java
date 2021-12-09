26d5701194443ce4840729f60c9994482233161a
klintdajao <73380066+klintdajao@users.noreply.github.com>
2021-12-09 10:40:32 +0800

    db

--- app/src/main/java/com/example/gittest/ui/orders/OrderFragment.java
+++ app/src/main/java/com/example/gittest/ui/orders/OrderFragment.java
@@ -7,13 +7,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 import com.example.gittest.DatabaseHelper;
-import com.example.gittest.FindId;
 import com.example.gittest.R;
 
 import java.text.DecimalFormat;
@@ -32,7 +30,6 @@ public class OrderFragment extends Fragment {
         Intent intent = getActivity().getIntent();
         DecimalFormat df = new DecimalFormat("#.##");
         db = new DatabaseHelper(getActivity());
-
 
 
 
