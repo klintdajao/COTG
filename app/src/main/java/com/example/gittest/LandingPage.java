58f75869537f39241c39e9ef90ccff66cbeeda2c
Totsukki <93961776+Totsukki@users.noreply.github.com>
2021-12-10 10:08:54 +0800

    verifyStoragePermissions

--- app/src/main/java/com/example/gittest/LandingPage.java
+++ app/src/main/java/com/example/gittest/LandingPage.java
@@ -5,12 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class LandingPage extends AppCompatActivity {
-    storagePermissions sp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        sp = new storagePermissions();
-        sp.verifyStoragePermissions(this);
         setContentView(R.layout.activity_landing_page);
 
     }
