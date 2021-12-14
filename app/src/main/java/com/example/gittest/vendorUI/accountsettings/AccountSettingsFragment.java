d43c50308006b18f553dd7e8d14a6cd7af4d5a23
Romel Kenneth Recabo <1rkennethr1@gmail.com>
2021-12-14 13:33:59 +0800

    initial commit

--- app/src/main/java/com/example/gittest/vendorUI/accountsettings/AccountSettingsFragment.java
+++ app/src/main/java/com/example/gittest/vendorUI/accountsettings/AccountSettingsFragment.java
@@ -1,21 +1,33 @@
 package com.example.gittest.vendorUI.accountsettings;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.gittest.AccountInfo;
+import com.example.gittest.DatabaseHelper;
+import com.example.gittest.EditAccount;
+import com.example.gittest.Login;
 import com.example.gittest.R;
+import com.example.gittest.VendorInfo;
+import com.example.gittest.loginID;
+import com.example.gittest.vendorUI.orders.OrdersFragment;
 
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link AccountSettingsFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class AccountSettingsFragment extends Fragment {
+public class AccountSettingsFragment extends Fragment implements View.OnClickListener {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -61,6 +73,60 @@ public class AccountSettingsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_account_settings, container, false);
+        View view =  inflater.inflate(R.layout.fragment_account_settings, container, false);
+        TextView txtVendorName2 = (TextView) view.findViewById(R.id.txtVendorName2);
+        TextView txtVendorID = (TextView) view.findViewById(R.id.txtVendorID);
+        TextView accountSettings = (TextView) view.findViewById(R.id.txtAccountSettings);
+        TextView performance = (TextView) view.findViewById(R.id.txtPerformance);
+        TextView orders = (TextView) view.findViewById(R.id.txtOrders);
+        TextView logout = (TextView) view.findViewById(R.id.txtLogOut);
+//        AccountInfo a = new VendorInfo();
+//        DatabaseHelper db = new DatabaseHelper(getActivity());
+//        a = db.readVendor(loginID.id);
+//        String name = a.getFn() +" " + a.getLn();
+//
+//        txtEmail.setText(a.getEmail());
+//        txtName.setText(name);
+        accountSettings.setOnClickListener(this);
+        performance.setOnClickListener(this);
+        orders.setOnClickListener(this);
+        logout.setOnClickListener(this);
+        return view;
+    }
+    @Override
+    public void onClick(View view) {
+        DatabaseHelper db = new DatabaseHelper(getActivity());
+        switch (view.getId()){
+            case R.id.txtAccountSettings:
+                Intent intent = new Intent(getActivity(), VendorInfo.class);
+                startActivity(intent);
+                break;
+//            case R.id.txtPerformance:
+//                Intent perf = new Intent(getActivity(), VPerformance.class);
+//                startActivity(perf);
+//                break;
+            case R.id.txtOrders:
+                Intent orders = new Intent(getActivity(), OrdersFragment.class);
+                startActivity(orders);
+                break;
+            case R.id.txtLogOut:
+                boolean delete = db.deleteUser(loginID.id);
+                AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+                builder.setMessage("Confirm Logout?")
+                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                if(delete){
+                                    Toast.makeText(getActivity(), "Vendor Logged Out Successfully", Toast.LENGTH_SHORT).show();
+                                    Intent in = new Intent(getActivity(), Login.class);
+                                    startActivity(in);
+                                }
+                            }
+                        }).setNegativeButton("No",null);
+
+                AlertDialog ad = builder.create();
+                ad.show();
+                break;
+        }
     }
 }
