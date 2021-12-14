9482e311a332f84c2c279cedd0fdae8e0028b7b9
klintdajao <73380066+klintdajao@users.noreply.github.com>
2021-12-14 12:05:40 +0800

    db

--- app/src/main/java/com/example/gittest/ProductDesc.java
+++ app/src/main/java/com/example/gittest/ProductDesc.java
@@ -2,13 +2,36 @@ package com.example.gittest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import java.io.File;
+import java.util.ArrayList;
 
 public class ProductDesc extends AppCompatActivity {
+
+    ImageView imgView;
+    DatabaseHelper db;
+    private ArrayList<Integer> mProdId = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product_desc);
+        mProdId = db.checkProdIDList();
+        imgView = findViewById(R.id.imgView);
+        db = new DatabaseHelper(this);
+
+
+        File imgFile[] = new File[mProdId.size()];
+        for (int i = 0; i < mProdId.size(); i++) {
+            imgFile[i] = new File(String.valueOf(db.checkProdImgURIList().get(i)));
+            if(imgFile[i].exists()){
+            Bitmap bitmap = BitmapFactory.decodeFile(imgFile[i].getAbsolutePath());
+            imgView.setImageBitmap(bitmap);
+            }
+        }
     }
 }
